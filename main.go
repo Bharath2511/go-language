f64ae64eda90663261f58963d5169fb27b118e1d
V S Bharath <bharathcse5t6@gmail.com>
2018-11-06 12:38:24 +0530

    shorthand for variables

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import "fmt"
 
 func main() {
   var age = 23
-  var name = "Bharath"
+  //var name = "Bharath"
+  // shorthand
+//   name := "Bharath"
+  size := 1.3
+  //var sizes float32 = 2.3
   var isCool = true
   isCool = false
-  fmt.Println(name,age)
+  
+   
+  name,email := "Bharath", "bharathchandra@gmal.com"
+
+  fmt.Println(name,age,email)
+
   //for getting the type
   fmt.Printf("%T\n", name)
   fmt.Printf("%T\n", age)
   fmt.Printf("%T\n", isCool)
+  fmt.Printf("%T\n", size)
 }
